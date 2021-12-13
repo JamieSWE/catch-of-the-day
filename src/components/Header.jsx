66f4fa5bb153332fd5c4224fdd54d6d237f42bbb
Jamie Shaw <jamieshaw94@gmail.com>
2021-12-13 18:51:42 -0500

    Set up routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+//destructuring of props
+const Header = ({ tagline }) => (
+  <header className='top'>
+    <h1>
+      Catch
+      <span className='ofThe'>
+        <span className='of'>of</span>
+        <span className='the'>the</span>
+      </span>
+      Day
+    </h1>
+    <h3 className='tagline'>
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
+
+export default Header;
+
+//WITHOUT DESTRUCTURING PROPS
+// const Header = (props) => {
+//   return (
+//     <header className='top'>
+//       <h1>
+//         Catch
+//         <span className='ofThe'>
+//           <span className='of'>of</span>
+//           <span className='the'>the</span>
+//         </span>
+//         Day
+//       </h1>
+//       <h3 className='tagline'>
+//         <span>{props.tagline}</span>
+//       </h3>
+//     </header>
+//   );
+// };
---
+++ src/components/Order.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div className='order'>
+        Order
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Order;
